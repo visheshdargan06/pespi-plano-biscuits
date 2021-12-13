1f3d4411a866ae9a0425507f4720f4a5ff5a1792
adt-tyagisumit <91182148+adt-tyagisumit@users.noreply.github.com>
2021-12-13 21:49:51 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # docker container exec yolo_container python src/copy_images.py
-docker container exec subbrand_container python /pepsi-plano-biscuits/src/main.py
-docker container exec yolo_container python /pepsi-plano-biscuits/src/update_table.py
+docker container exec pepsi_latum_biscuit_planogram_container python /pepsi-plano-biscuits/src/main.py
+#docker container exec yolo_container python /pepsi-plano-biscuits/src/update_table.py
