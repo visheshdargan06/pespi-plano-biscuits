fac4196625575381f8a35b9a2631eaa815920d27
adt-tyagisumit <91182148+adt-tyagisumit@users.noreply.github.com>
2021-12-13 21:43:45 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,3 @@
+# docker container exec yolo_container python src/copy_images.py
+docker container exec subbrand_container python /pepsi-plano-biscuits/src/main.py
+docker container exec yolo_container python /pepsi-plano-biscuits/src/update_table.py
